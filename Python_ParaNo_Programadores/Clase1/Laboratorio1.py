415f0920882e837d7f72cef7f0346cbabf638f9f
lmoreno98 <lucho_moreno@live.com.ar>
2021-05-17 21:01:54 -0300

    Creacion de los dos laboratorios

---
+++ Python_ParaNo_Programadores/Clase1/Laboratorio1.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+
+suma = numero1 + numero2
+
+print(f"La suma entre {numero1} + {numero2} = {suma}")
