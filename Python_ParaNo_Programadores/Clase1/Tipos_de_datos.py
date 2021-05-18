ca00773bf6a6bb7abedc2793f6c2adedaeba435a
lmoreno98 <lucho_moreno@live.com.ar>
2021-05-17 21:44:24 -0300

    Creacion de tipos de datos

---
+++ Python_ParaNo_Programadores/Clase1/Tipos_de_datos.py
@@ -0,0 +1,13 @@
+
+
+# Estos son tipos de datos alfabeticos
+print("Hola mundo")
+print("### Chau")
+print("!!!!")
+
+
+# Estos son tipos de datos numericos
+
+print("3234")
+print("658")
+print("123")
