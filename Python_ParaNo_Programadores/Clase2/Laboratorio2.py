a00cea9cc8a135acebeba1d862a216944acdfd01
lmoreno98 <lucho_moreno@live.com.ar>
2021-05-21 21:46:34 -0300

    Creacion de la clase 2 de python digitalers

---
+++ Python_ParaNo_Programadores/Clase2/Laboratorio2.py
@@ -0,0 +1,8 @@
+print("Verificador de ingreso de palabra")
+
+nombre = input("Ingrese su nombre: ")
+
+if nombre != "":
+    print("El nombre se ingreso correctamente")
+else:
+    print("Error no se ingreso un nombre valido")
